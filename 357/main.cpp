3fee0e3644b3120aaf4053ea9ca49e77070fb76d
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2019-11-27 13:48:45 -0500

    Auto format

--- 357/main.cpp
+++ 357/main.cpp
@@ -15,30 +15,38 @@
 using namespace std;
 
 // Memoize primes
-bool primeMemo[50000000] = {false}; // Primes are true, not checked/not prime are false
+bool primeMemo[50000000] = {false};    // Primes are true, not checked/not prime are false
 bool notPrimeMemo[50000000] = {false}; // Only not prime are true, not checked/prime are false
 
-// Algorithm to determine primality of a number, based off method I used in https://github.com/ddigeronimo/CS546/blob/master/lab_09/public/js/prime.js 
+// Algorithm to determine primality of a number, based off method I used in https://github.com/ddigeronimo/CS546/blob/master/lab_09/public/js/prime.js
 // As opposed to my source, this method uses memoization to increase efficiency as the program runs
-bool isPrime(int n) {
+bool isPrime(int n)
+{
     // Check memo array
-    if (primeMemo[n]) {
+    if (primeMemo[n])
+    {
         return true;
     }
-    if (notPrimeMemo[n]) {
+    if (notPrimeMemo[n])
+    {
         return false;
     }
-    if (n == 2 || n == 3) {
+    if (n == 2 || n == 3)
+    {
         primeMemo[n] = true;
         return true;
-    } else if (n % 2 == 0 || n % 3 == 0) {
+    }
+    else if (n % 2 == 0 || n % 3 == 0)
+    {
         notPrimeMemo[n] = true;
         return false;
     }
     int i = 5;
     int w = 2;
-    while (i * i <= n) {
-        if (n % i == 0) {
+    while (i * i <= n)
+    {
+        if (n % i == 0)
+        {
             notPrimeMemo[n] = true;
             return false;
         }
@@ -50,26 +58,36 @@ bool isPrime(int n) {
 }
 
 // Given a number n, do all n's divisors d follow the property (d + n / d is prime)?
-// TODO: Optimize this
-bool areDivisorsValid(int n) {
+// TODO: Optimize this --> Less loops
+bool areDivisorsValid(int n)
+{
     // First, loop through every number i up to half of n to check if it is a divisor
     // It's pointless to check past the halfway point, as no number is divisible by any number that is greater than 50% of itself
-
-    // If n is odd, we can check only odd numbers for its divisors
-    if (n % 2 == 0) {
-        for (int i = 1; i <= n / 2; i+=2) {
+    if (n % 2 == 0)
+    {
+        // If n is odd, we can check only odd numbers for its divisors
+        for (int i = 1; i <= n / 2; i += 2)
+        {
             // If i is a divisor, we must check its validity
-            if (n % i == 0) {
-                if (!isPrime(i + n / i)) {
+            if (n % i == 0)
+            {
+                if (!isPrime(i + n / i))
+                {
                     return false;
                 }
             }
         }
-    } else {
-        for (int i = 1; i <= n / 2; i++) {
+    }
+    else
+    {
+        // However, if n is even, we need to check for odd and even divisors
+        for (int i = 1; i <= n / 2; i++)
+        {
             // If i is a divisor, we must check its validity
-            if (n % i == 0) {
-                if (!isPrime(i + n / i)) {
+            if (n % i == 0)
+            {
+                if (!isPrime(i + n / i))
+                {
                     return false;
                 }
             }
@@ -78,7 +96,8 @@ bool areDivisorsValid(int n) {
     return true;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Fast I/O trick
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -91,14 +110,16 @@ int main(int argc, char *argv[]) {
     // Check the sutibility of all even numbers through 100000000 and add them onto a sum value
     // No odd numbers other than 1 are valid, so we can skip them and start the sum value at one
     double sumOfValidNumbers = 1;
-    for (int i = 2; i < 100000000; i+=2) {
-        if (areDivisorsValid(i)) {
+    for (int i = 2; i < 100000000; i += 2)
+    {
+        if (areDivisorsValid(i))
+        {
             printf("%d is valid\n", i);
-            sumOfValidNumbers += (double) i;
+            sumOfValidNumbers += (double)i;
         }
     }
 
-    duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+    duration = (clock() - start) / (double)CLOCKS_PER_SEC;
     printf("Sum of all valid numbers: %f\n", sumOfValidNumbers);
     printf("Total time: %f seconds\n", duration);
 
